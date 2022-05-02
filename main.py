6166ccc79c961e635218305cb1245ea5b3b1cc07
Stefano Bavaro <hufflepuffc@gmail.com>
2022-05-02 10:32:25 +0200

    Added w2v embedding and w2v experiments

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ case_name = "Case ID"
 timestamp_name = "Complete Timestamp"
 outcome_name = "label"
 win_size = 4
-net_out = 0 #0 = double output ; 1 = nextActivity net ; 2= outcome net
-net_embedding= 1 #0 = embedding, 1 = word_to_vec
+net_out = 2 #0 = double output ; 1 = nextActivity net ; 2= outcome net
+net_embedding= 1 #0 = embedding, 1 = word2vec
 
 
 manager = Manager(log_name, activity_name, case_name, timestamp_name, outcome_name,win_size, net_out, net_embedding)
@@ -45,130 +45,132 @@ manager.csv_to_data()
 
 algorithm = tpe.suggest
 trials = Trials()
-#
-# if(net_out==0):
-#     search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
-#                     'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
-#                     'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
-#                     'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
-#                     'n_layers': hp.choice('n_layers', [
-#                     {'n_layers': 1},
-#                     {'n_layers': 2,
-#                         'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
-#                         'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
-#                      },
-#                     {'n_layers': 3,
-#                         'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
-#                         'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
-#                         'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
-#                         'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))}
-#                     ]),
-#                     'gamma': hp.uniform("gamma", 0.1,0.9),
-#                     'dropout': hp.uniform("dropout", 0, 0.5),
-#                     'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
-#                     'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
-#                     }
-#     outfile = open('../Progetto-Tesi/data/log_files/' + log_name +'_'+net_embedding +'_doubleOutput.log', 'w')
-# elif(net_out==1):
-#     search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
-#                     'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
-#                     'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
-#                     #'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
-#                     'n_layers': hp.choice('n_layers', [
-#                     {'n_layers': 1},
-#                     {'n_layers': 2,
-#                         'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
-#                         #'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
-#                      },
-#                     {'n_layers': 3,
-#                         'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
-#                         #'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
-#                         'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
-#                         #'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))
-#                      }
-#                     ]),
-#                     'win_size':4,
-#                     'dropout': hp.uniform("dropout", 0, 0.5),
-#                     'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
-#                     'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
-#                     }
-#     outfile = open('../Progetto-Tesi/data/log_files/' + log_name+'_'+net_embedding + '_singleActOutput.log', 'w')
-#
-# elif(net_out==2):
-#      search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
-#                      'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
-#                     #'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
-#                     'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
-#                     'n_layers': hp.choice('n_layers', [
-#                     {'n_layers': 1},
-#                     {'n_layers': 2,
-#                         #'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
-#                         'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
-#                      },
-#                     {'n_layers': 3,
-#                         #'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
-#                         'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
-#                         #'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
-#                         'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))
-#                      }
-#                     ]),
-#                     'win_size': 4,
-#                     #'gamma': hp.uniform("gamma", 0.1,0.9),
-#                     'dropout': hp.uniform("dropout", 0, 0.5),
-#                     'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
-#                     'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
-#                     }
-#      outfile = open('../Progetto-Tesi/data/log_files/' + log_name +'_'+net_embedding+ '_singleOutOutput.log', 'w')
-#
-# best_params = fmin(
-#       fn=manager.nn,
-#       space=search_space,
-#       algo=algorithm,
-#       max_evals=20,
-#       trials=trials)
-# print(len(trials))
-#
-# best_params = space_eval(search_space,best_params)
-# print(best_params)
-#
-# outfile.write("\nHyperopt trials:")
-# outfile.write("\ntid,loss,params used")
-# for trial in trials.trials:
-#     #print(trial)
-#     outfile.write("\n%d, %f, %s" % (trial['tid'],
-#                             trial['result']['loss'],
-#                             trial['misc']['vals']))
-#
-#
-# outfile.write("\n\nBest parameters:")
-# print(best_params, file=outfile)
-#
-# manager.best_model.save("model/generate_" + log_name + str(net_out) + ".h5")
-#
-# print('Evaluating final model...')
-# model= load_model("model/generate_" + log_name + str(net_out)+".h5")
-# if(net_out==0):
-#     reportNA,cmNA,reportO,cmO = manager.evaluate_model(model)
-#     outfile.write("\nNext activity metrics:\n")
-#     print(reportNA, file=outfile)
-#     outfile.write("\nNext activity confusion matrix:\n")
-#     print(cmNA, file=outfile)
-#     outfile.write("\nOutcome metrics:\n")
-#     print(reportO, file=outfile)
-#     outfile.write("\nOutcome confusion matrix:\n")
-#     print(cmO, file=outfile)
-# elif(net_out==1):
-#     reportNA,cmNA = manager.evaluate_model(model)
-#     outfile.write("\nNext activity metrics:\n")
-#     print(reportNA, file=outfile)
-#     outfile.write("\nNext activity confusion matrix:\n")
-#     print(cmNA, file=outfile)
-# elif(net_out==2):
-#     reportO,cmO = manager.evaluate_model(model)
-#     outfile.write("\nOutcome metrics:\n")
-#     print(reportO, file=outfile)
-#     outfile.write("\nOutcome confusion matrix:\n")
-#     print(cmO, file=outfile)
+
+if(net_out==0):
+    search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
+                    'word2vec_size': hp.uniformint('word2vec_size',32,1024),
+                    'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
+                    'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
+                    'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
+                    'n_layers': hp.choice('n_layers', [
+                    {'n_layers': 1},
+                    {'n_layers': 2,
+                        'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
+                        'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
+                     },
+                    {'n_layers': 3,
+                        'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
+                        'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
+                        'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
+                        'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))}
+                    ]),
+                    'gamma': hp.uniform("gamma", 0.1,0.9),
+                    'dropout': hp.uniform("dropout", 0, 0.5),
+                    'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
+                    'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
+                    }
+    outfile = open('../Progetto-Tesi/data/log_files/' + log_name +'_'+str(net_embedding) +'_doubleOutput.log', 'w')
+elif(net_out==1):
+    search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
+                    'word2vec_size': hp.uniformint('word2vec_size',32,1024),
+                    'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
+                    'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
+                    #'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
+                    'n_layers': hp.choice('n_layers', [
+                    {'n_layers': 1},
+                    {'n_layers': 2,
+                        'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
+                        #'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
+                     },
+                    {'n_layers': 3,
+                        'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
+                        #'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
+                        'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
+                        #'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))
+                     }
+                    ]),
+                    'win_size':4,
+                    'dropout': hp.uniform("dropout", 0, 0.5),
+                    'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
+                    'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
+                    }
+    outfile = open('../Progetto-Tesi/data/log_files/' + log_name+'_'+str(net_embedding) + '_singleActOutput.log', 'w')
+elif(net_out==2):
+     search_space = {'output_dim_embedding':scope.int(hp.loguniform('output_dim_embedding', np.log(10), np.log(150))),
+                     'shared_lstm_size': scope.int(hp.loguniform('shared_lstm_size', np.log(10), np.log(150))),
+                     'word2vec_size': hp.uniformint('word2vec_size',32,1024),
+                    #'lstmA_size_1':  scope.int(hp.loguniform('lstmA_size_1', np.log(10), np.log(150))),
+                    'lstmO_size_1':  scope.int(hp.loguniform('lstmO_size_1', np.log(10), np.log(150))),
+                    'n_layers': hp.choice('n_layers', [
+                    {'n_layers': 1},
+                    {'n_layers': 2,
+                        #'lstmA_size_2_2': scope.int(hp.loguniform('lstmA_size_2_2', np.log(10), np.log(150))),
+                        'lstmO_size_2_2': scope.int(hp.loguniform('lstmO_size_2_2', np.log(10), np.log(150))),
+                     },
+                    {'n_layers': 3,
+                        #'lstmA_size_2_3': scope.int(hp.loguniform('lstmA_size_2_3', np.log(10), np.log(150))),
+                        'lstmO_size_2_3': scope.int(hp.loguniform('lstmO_size_2_3', np.log(10), np.log(150))),
+                        #'lstmA_size_3_3': scope.int(hp.loguniform('lstmA_size_3_3', np.log(10), np.log(150))),
+                        'lstmO_size_3_3': scope.int(hp.loguniform('lstmO_size_3_3', np.log(10), np.log(150)))
+                     }
+                    ]),
+                    'win_size': 4,
+                    #'gamma': hp.uniform("gamma", 0.1,0.9),
+                    'dropout': hp.uniform("dropout", 0, 0.5),
+                    'batch_size': scope.int(hp.uniform('batch_size', 3, 6)),
+                    'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))
+                    }
+     outfile = open('../Progetto-Tesi/data/log_files/' + log_name +'_'+str(net_embedding)+ '_singleOutOutput.log', 'w')
+
+best_params = fmin(
+      fn=manager.nn,
+      space=search_space,
+      algo=algorithm,
+      max_evals=20,
+      trials=trials)
+print(len(trials))
+
+best_params = space_eval(search_space,best_params)
+print(best_params)
+
+outfile.write("\nHyperopt trials:")
+outfile.write("\ntid,loss,params used")
+for trial in trials.trials:
+    #print(trial)
+    outfile.write("\n%d, %f, %s" % (trial['tid'],
+                            trial['result']['loss'],
+                            trial['misc']['vals']))
+
+
+outfile.write("\n\nBest parameters:")
+print(best_params, file=outfile)
+
+manager.best_model.save("models/generate_" + log_name + "_type"+str(net_out)+"_emb"+str(net_embedding) + ".h5")
+
+print('Evaluating final models...')
+model= load_model("models/generate_" + log_name + "_type"+str(net_out)+"_emb"+str(net_embedding) + ".h5")
+if(net_out==0):
+    reportNA,cmNA,reportO,cmO = manager.evaluate_model(model,best_params['word2vec_size'])
+    outfile.write("\nNext activity metrics:\n")
+    print(reportNA, file=outfile)
+    outfile.write("\nNext activity confusion matrix:\n")
+    print(cmNA, file=outfile)
+    outfile.write("\nOutcome metrics:\n")
+    print(reportO, file=outfile)
+    outfile.write("\nOutcome confusion matrix:\n")
+    print(cmO, file=outfile)
+elif(net_out==1):
+    reportNA,cmNA = manager.evaluate_model(model,best_params['word2vec_size'])
+    outfile.write("\nNext activity metrics:\n")
+    print(reportNA, file=outfile)
+    outfile.write("\nNext activity confusion matrix:\n")
+    print(cmNA, file=outfile)
+elif(net_out==2):
+    reportO,cmO = manager.evaluate_model(model,best_params['word2vec_size'])
+    outfile.write("\nOutcome metrics:\n")
+    print(reportO, file=outfile)
+    outfile.write("\nOutcome confusion matrix:\n")
+    print(cmO, file=outfile)
 
 
 
